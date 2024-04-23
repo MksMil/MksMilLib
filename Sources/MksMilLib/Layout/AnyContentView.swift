b0bb7bb76d3e720e33fc8eb5d8b26e691d45f9c1
MksMil <maksymmiliaiev@gmail.com>
2024-04-23 08:02:39 +0300

    change animation duration value for AnyCotentView change state animation

--- Sources/MksMilLib/Layout/AnyContentView.swift
+++ Sources/MksMilLib/Layout/AnyContentView.swift
@@ -99,7 +99,7 @@ public struct AnyContentView<T: View,B:View,But: View,Prompt: View, SelectableCo
                         }
                     }
                     .onPreferenceChange(AnyContentViewSizePreferenceKey.self, perform: { val in
-                            withAnimation(.easeInOut(duration: editMode ? 0.2: 0.4)) {
+                            withAnimation(.easeInOut(duration: editMode ? 0.4: 0.8)) {
                                 self.totalHeight = val.height
                             }
                     })
@@ -112,7 +112,7 @@ public struct AnyContentView<T: View,B:View,But: View,Prompt: View, SelectableCo
             }
             
             Button(action: {
-                withAnimation(.easeInOut(duration: editMode ? 0.2: 0.4)){
+                withAnimation(.easeInOut(duration: editMode ? 0.4: 0.8)){
                     editMode.toggle()
                     allCases = editMode ? identableContent : filteredContent()
                     selectedContent = selectedCases.map { $0.0 }
